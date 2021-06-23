2936f152b2c2e9bebeb3aa060621b3dacca1b7ff
Hong-Sen <hse025@naver.com>
2021-06-24 07:00:49 +0900

    Feature bottom navi selector color
    -click : white
    -unclick: black

--- app/src/main/java/kr/sswu/whydomyplantsdie/Fragment/AlarmFragment.java
+++ app/src/main/java/kr/sswu/whydomyplantsdie/Fragment/AlarmFragment.java
@@ -17,5 +17,6 @@ public class AlarmFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_alarm, container, false);
+        
     }
 }
