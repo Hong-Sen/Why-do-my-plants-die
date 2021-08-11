f27af4cf65d10ed80a229edfda57d06d2f884363
kimjeonghui <59547003+kimjeonghui@users.noreply.github.com>
2021-08-11 21:22:57 +0900

    Revert "Feat MBTI webview"
    This reverts commit 9483322506c817d1337ee29ea03ce90e075a046c.

--- app/src/main/java/kr/sswu/whydomyplantsdie/Fragment/MbtiFragment.java
+++ app/src/main/java/kr/sswu/whydomyplantsdie/Fragment/MbtiFragment.java
@@ -1,12 +1,9 @@
 package kr.sswu.whydomyplantsdie.Fragment;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,31 +15,6 @@ public class MbtiFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_mbti, container, false);
-
-        WebView webView = view.findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://whydomyplantdie.netlify.app/");
-        webView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction() == KeyEvent.ACTION_DOWN){
-                    if (keyCode == KeyEvent.KEYCODE_BACK){
-                        if (webView!=null){
-                            if (webView.canGoBack()){
-                                webView.goBack(); }
-                            else {
-                                getActivity().onBackPressed();
-                            }
-                        }
-                    }
-                }
-                return true;
-            }
-        });
-
-        return view;
-
+        return inflater.inflate(R.layout.fragment_mbti, container, false);
     }
 }
